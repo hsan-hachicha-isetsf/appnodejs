c5d2a6e8884efe04144c432b478a5e4d851f3b59
Hachicha hassan <hsan.hachicha@gmail.com>
2025-05-25 19:46:43 +0200

    Commit new version

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Application DevOps!');
+  res.send('Application DevOps annéé 2025!');
+});
+app.get('/contact', (req, res) => {
+  res.send('page contact!');
 });
 app.listen(3000, () => console.log('App running on port 3000'));
